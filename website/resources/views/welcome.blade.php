fdaff7e02c93ddcf82de176431d5a1fe2f3323f6
RSmey <nouraksmeyrs@gmail.com>
2019-06-05 11:26:36 +0700

    v-1 test

--- website/resources/views/welcome.blade.php
+++ website/resources/views/welcome.blade.php
@@ -70,7 +70,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Let Travel API v1.0 test
+                    Let Travel API v1.0 Release V-1
                 </div>
             </div>
             
