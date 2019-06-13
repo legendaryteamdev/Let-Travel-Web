4d661f6e3dd15b69e7f1baf4ff16bfc0f1ab4a8d
NouRaksmey <nouraksmeyrs@gmail.com>
2019-06-13 19:21:32 +0700

    last_test

--- website/resources/views/welcome.blade.php
+++ website/resources/views/welcome.blade.php
@@ -70,7 +70,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Let Travel API v1.0 Release V-1
+                    Let Travel API v1.0 Release V-1 Combination
                 </div>
             </div>
             
